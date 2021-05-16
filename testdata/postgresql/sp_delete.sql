c844f28075035539ad4e44b7f501a9f8413a2a61
Cristiano Colangelo <cristiano.colangelo@bluewin.ch>
2021-05-16 15:32:48 +0200

    Update sp_delete for psql

--- testdata/postgresql/sp_delete.sql
+++ testdata/postgresql/sp_delete.sql
@@ -1,14 +1,9 @@
 create or replace function sp_delete(k8sName text) returns void as $func$
 begin
-	
--- drop any active connection to the db to avoid error	
-select pg_terminate_backend(pg_stat_activity.pid)
-from pg_stat_activity
-where pg_stat_activity.datname = 'target_db' and pid <> pg_backend_pid();
 
 if exists (select 1 from pg_database where datname = k8sName) then
-	perform dblink_exec('dbname=' || current_database(), 'DROP DATABASE ' || quote_ident(k8sName));
+	perform dblink_exec('dbname=' || current_database(), 'DROP DATABASE ' || quote_ident(k8sName) || ' WITH (FORCE)');
 end if;
-return;
+
 end;
 $func$ language plpgsql;
