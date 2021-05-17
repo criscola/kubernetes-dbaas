f93e433f54c6ef4d42a4593e77670926e7a20e6e
Cristiano Colangelo <cristiano.colangelo@bluewin.ch>
2021-05-17 14:19:36 +0200

    Add postgres driver test

---
+++ pkg/database/psql_test.go
@@ -0,0 +1,56 @@
+package database_test
+
+import (
+	"github.com/bedag/kubernetes-dbaas/pkg/database"
+	. "github.com/bedag/kubernetes-dbaas/pkg/test"
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+)
+
+const psqlCreateOpName = "sp_create_db_rowset_eav"
+
+var _ = Describe(FormatTestDesc(Integration, "Postgres CreateDb"), func() {
+	// Setting up connection to DBMS
+	dsn, err := database.Dsn("postgres://postgres:Password&1@localhost:5432").GenPostgres()
+	Expect(err).ToNot(HaveOccurred())
+
+	conn, err := database.NewPsqlConn(dsn)
+	Expect(err).ToNot(HaveOccurred())
+	Context("when Operation is defined correctly", func() {
+		// Prepare test data
+		createOperation := database.Operation{
+			Name: psqlCreateOpName,
+			Inputs: map[string]string{
+				"k8sName": "myTestDb",
+			},
+		}
+
+		// Prepare assertion data
+		opResultAssertion := database.OpOutput{
+			Result: map[string]string{
+				"username": "testuser",
+				"password": "testpassword",
+				"dbName": "myTestDb",
+				"fqdn": "localhost",
+				"port": "5432",
+			},
+			Err: nil,
+		}
+
+		// Execute tested operation
+		var result database.OpOutput
+		result = conn.CreateDb(createOperation)
+
+		It("should not return an error", func() {
+			Expect(result.Err).ToNot(HaveOccurred())
+		})
+
+		It("should return a non-nil stored procedure Result", func() {
+			Expect(result.Result).ToNot(BeNil())
+		})
+
+		It("should return a rowset as specified in the stored procedure", func() {
+			Expect(result).To(Equal(opResultAssertion))
+		})
+	})
+})
